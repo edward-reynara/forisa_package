130546c7e7e263a5aed4b5ff3e0e7a4bccff6841
edward reynara <edwardreynara@gmail.com>
2024-02-22 15:09:50 +0700

    screen_argument_model

--- lib/models/screen_argument_model.dart
+++ lib/models/screen_argument_model.dart
@@ -27,18 +27,18 @@ class ScreenArgument {
 
     factory ScreenArgument.fromJson(Map<String, dynamic> json) => ScreenArgument(
         id: json["id"] == null ? null : json["id"] is int ? json["id"].toString():json["id"],
-        stringPayload: json["stringPayload"] == null ? null : json["stringPayload"],
-        pageTitle: json["pageTitle"] == null ? null : json["pageTitle"],
-        link: json["link"] == null ? null : json["link"],
+        stringPayload: json["stringPayload"],
+        pageTitle: json["pageTitle"],
+        link: json["link"],
         objectPayload: json["objectPayload"],
         listPayload: json["listPayload"] == null ? null : List<dynamic>.from(json["listPayload"].map((x) => x)),
     );
 
     Map<String, dynamic> toJson() => {
-        "id": id == null ? null : id,
-        "stringPayload": stringPayload == null ? null : stringPayload,
-        "pageTitle": pageTitle == null ? null : pageTitle,
-        "link": link == null ? null : link,
+        "id": id,
+        "stringPayload": stringPayload,
+        "pageTitle": pageTitle,
+        "link": link,
         "objectPayload": objectPayload,
         "listPayload": listPayload == null ? null : List<dynamic>.from(listPayload!.map((x) => x)),
     };
