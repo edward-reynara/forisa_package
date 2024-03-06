35233844cbcd3a49ffb351776de46c659c296395
edward reynara <edwardreynara@gmail.com>
2024-03-06 10:38:48 +0700

    Update app_model.dart

--- lib/models/app_model.dart
+++ lib/models/app_model.dart
@@ -20,15 +20,15 @@ class AppModel {
     AppModelData? data;
 
     factory AppModel.fromJson(Map<String, dynamic> json) => AppModel(
-        responseCode: json["response_code"],
-        responseMsg: json["response_msg"],
+        responseCode: json["response_code"] == null ? null : json["response_code"],
+        responseMsg: json["response_msg"] == null ? null : json["response_msg"],
         data: json["data"] == null ? null : AppModelData.fromJson(json["data"]),
     );
 
     Map<String, dynamic> toJson() => {
-        "response_code": responseCode,
-        "response_msg": responseMsg,
-        "data": data?.toJson(),
+        "response_code": responseCode == null ? null : responseCode,
+        "response_msg": responseMsg == null ? null : responseMsg,
+        "data": data == null ? null : data!.toJson(),
     };
 }
 
@@ -46,16 +46,16 @@ class AppModelData {
     String? storeUrl;
 
     factory AppModelData.fromJson(Map<String, dynamic> json) => AppModelData(
-        appcode: json["appcode"],
-        minVersion: json["min_version"],
-        latestVersion: json["latest_version"],
-        storeUrl: json["store_url"],
+        appcode: json["appcode"] == null ? null : json["appcode"],
+        minVersion: json["min_version"] == null ? null : json["min_version"],
+        latestVersion: json["latest_version"] == null ? null : json["latest_version"],
+        storeUrl: json["store_url"] == null ? null : json["store_url"],
     );
 
     Map<String, dynamic> toJson() => {
-        "appcode": appcode,
-        "min_version": minVersion,
-        "latest_version": latestVersion,
-        "store_url": storeUrl,
+        "appcode": appcode == null ? null : appcode,
+        "min_version": minVersion == null ? null : minVersion,
+        "latest_version": latestVersion == null ? null : latestVersion,
+        "store_url": storeUrl == null ? null : storeUrl,
     };
 }
