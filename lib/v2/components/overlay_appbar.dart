816ae1205d3840ed3ac00b66d6d1d922d64dc4f9
edward reynara <edwardreynara@gmail.com>
2024-04-22 15:45:59 +0700

    Update overlay_appbar.dart

--- lib/v2/components/overlay_appbar.dart
+++ lib/v2/components/overlay_appbar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import '../configs/config.dart';
 
 AppBar overlayAppBar(BuildContext context) {
